24fddd75846bb283983c35ab03a33dbe886949d0
Aissamab <aissamabakkar19@@gmail.com>
2023-08-15 05:20:05 -0700

    this is my first commit fot the day

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,7 +15,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
@@ -24,9 +24,10 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	else if (n < 0)
+	else (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
+	_putchar('\n');
 }
