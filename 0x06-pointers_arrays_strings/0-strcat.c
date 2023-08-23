fec7401c3b0a1382d896887e928ab65c43d624ca
Aissamab <aissamabakkar19@@gmail.com>
2023-08-23 11:26:37 -0700

    this the first commit of the day

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,15 +5,17 @@
  * @src: input value
  *
  * Return: void
-*/
+ */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
+	{
 		i++;
-	
+	}
 	j = 0;
 	while (src[j] != '\0')
 	{
@@ -21,6 +23,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
