d3184fcb7fb397326e43cce56fc605e1db0bad70
Aissamab <aissamabakkar19@@gmail.com>
2023-08-28 05:10:37 -0700

    first commit of the day

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
@@ -18,6 +18,6 @@ char *_strchr(char *s, char c)
 		}
 	}
 
-	return (0);
+	return ('\0');
 
 }
