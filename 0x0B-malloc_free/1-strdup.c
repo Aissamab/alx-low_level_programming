712f976d12a104b93aa7653d89bfe22ab21bea74
Aissamab <aissamabakkar19@@gmail.com>
2023-09-04 04:29:07 -0700

    this is the first commit of the day

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i, size;
+	char *cpy;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+		;
+
+	cpy = malloc(size * sizeof(*str) + 1);
+
+	if (cpy == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cpy[i] = str[i];
+	return (str);
+}
