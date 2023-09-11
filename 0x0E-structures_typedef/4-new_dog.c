9ff6e6520a683b25ab47cbcc6ed53ccab9a2358d
Aissamab <aissamabakkar19@@gmail.com>
2023-09-11 04:55:03 -0700

    this is the first commit of the day

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,15 +9,10 @@
  */
 int _strlen(char *p)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (p[i] != '\0')
-	{
+	while (*p++)
 		i++;
-	}
-
 	return (i);
 }
 
@@ -26,23 +21,14 @@ int _strlen(char *p)
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
  *
- * Return: the pointer to dest
+ * Return: @dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int i;
 
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
+	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
-	}
 	dest[i] = '\0';
 
 	return (dest);
@@ -59,31 +45,31 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int len1, len2;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
+	if (!name || age < 0 || !owner)
+		return (NULL);
 
-	dog = malloc(sizeof(dog_t));
+	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (dog->owner == NULL)
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dog).owner == NULL)
 	{
 		free(dog);
 		free(dog->name);
 		return (NULL);
 	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
+
+	dog->name = _strcpy(dog->name, name);
 	dog->age = age;
+	dog->owner = _strcpy(dog->owner, owner);
 
 	return (dog);
 }
