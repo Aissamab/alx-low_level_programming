e8693c1fc1701749ef608827400135229248bae4
Aissamab <aissamabakkar19@@gmail.com>
2023-09-13 04:35:32 -0700

    this is the first commit of the day

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,16 +8,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (array && size && cmp)
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		while (i < size)
-		{
-			if (cmp(array[i])
-				return (i);
-			i++;
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
