1c4a5b58e7d82f0ae367f80af93c95486e385abf
Aissamab <aissamabakkar19@@gmail.com>
2023-11-09 12:00:49 -0800

    this is the first commit of the day

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - prints the length of a doubly linked list
+ * @h: address to the head
+ * Return: the length
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
